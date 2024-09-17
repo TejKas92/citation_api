997328470db1d9efd3c37bc60b6316f0e07f7b49
Tej <kasmi.tejeddine@gmail.com>
2024-09-17 14:26:42 +0200

    db connection fixed

--- database/database.py
+++ database/database.py
@@ -1,11 +1,18 @@
-from sqlalchemy import create_engine
+from sqlalchemy import URL, create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 
 import os
 
+connection_string = URL.create(
+    'postgresql',
+    username=os.environ.get("DB_USER"),
+    password=os.environ.get("DB_PASS"),
+    host=os.environ.get("DB_HOST"),
+    database=os.environ.get("DB_NAME"),
+)
 
-SQLALCHEMY_DATABASE_URL = os.environ.get("DB_CONFIG")
-engine = create_engine(SQLALCHEMY_DATABASE_URL)
+engine = create_engine(connection_string)
+
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 Base = declarative_base()
