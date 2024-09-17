ed8376abdc1192ed2e58328f1e40ea9801c5f5f6
Tej <kasmi.tejeddine@gmail.com>
2024-09-17 12:55:07 +0200

    secret management

--- database.py
+++ database.py
@@ -2,15 +2,10 @@ from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 
-from dotenv import load_dotenv
 import os
 
-load_dotenv()
 
-db_user = os.getenv('DB_USER')
-db_pass = os.getenv('DB_PASS')
-
-SQLALCHEMY_DATABASE_URL = f"postgresql://{db_user}:{db_pass}@localhost/citation"
+SQLALCHEMY_DATABASE_URL = os.environ.get("DB_CONFIG")
 engine = create_engine(SQLALCHEMY_DATABASE_URL)
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 Base = declarative_base()
